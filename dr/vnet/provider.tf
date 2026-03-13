d61385dd51dcfa6c3631530317b222eae06e417c
Aayu1983 <aayusinghal1983@gmail.com>
2026-03-13 09:44:10 +0530

    Update provider.tf

--- dr/vnet/provider.tf
+++ dr/vnet/provider.tf
@@ -1,15 +1,25 @@
 terraform {
+  required_version = ">= 1.5.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.14.0"
+      version = "~> 4.0"
     }
+  }
+  backend "azurerm" {
+    resource_group_name  = "myrg"      
+    storage_account_name = "mydemostg"   
+    container_name       = "mycontainer"             
+    key                  = "production.terraform.tfstate"
   }
 }
 
 provider "azurerm" {
-  features {
-
-  }
-  subscription_id = var.subscription_id
+  subscription_id = var.main_subscription_id
+  features {}
+}
+provider "azurerm" {
+  alias           = "storage_sub"
+  subscription_id = var.storage_subscription_id
+  features {}
 }
