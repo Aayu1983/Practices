22a714a82ff5a83ba647a12d4523ad8060bb9e57
Aayu1983 <aayusinghal1983@gmail.com>
2026-03-15 23:58:22 +0530

    Update variable.tf

--- dr/vnet/variable.tf
+++ dr/vnet/variable.tf
@@ -6,12 +6,4 @@ variable "Vnet" {
     resource_group_name = string
   }))
 }
-variable "main_subscription_id" {
-  type        = string
-  description = "Subscription ID of Resources deployment"
-}
 
-variable "storage_subscription_id" {
-  type        = string
-  description = "Subscription ID of Storage Account"
-}
